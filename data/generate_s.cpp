270ed565f75837c6c653a9c053a7ad1aa2c2e509
Satria H R Harsono <HafizhHarsono@gmail.com>
2018-05-18 02:14:58 +0700

    Add name file customized by p and q

--- data/generate_s.cpp
+++ data/generate_s.cpp
@@ -3,23 +3,30 @@
 #include <iterator>
 #include <numeric>
 #include <set>
+#include <sstream>
 #include <unordered_set>
 
-void generate(const char * pqsFilePath="", const char * primeFilePath="") {
+void generate(uint16_t pFrom, uint16_t pUntil, const char * pqsFilePath="", const char * primeFilePath="") {
   std::set<uint16_t> primes;
   std::string line;
-  primeFilePath = primeFilePath == ""? "./data/primes_2_bytes_for_p_q" : primeFilePath;
+  primeFilePath = primeFilePath == ""? "./data/primes_2_bytes_for_p_q_fake" : primeFilePath;
   std::ifstream primeFile(primeFilePath);
   while (std::getline(primeFile, line)) {
     primes.insert(std::stoi(line));
   }
   primeFile.close();
 
+  std::set<uint16_t>::iterator ipFrom = primes.find(pFrom);
+  std::set<uint16_t>::iterator ipUntil = primes.find(pUntil);
   std::set<uint16_t>::iterator end = primes.end();
   std::set<uint16_t>::iterator beforeEnd = primes.end()--;
+
   pqsFilePath = pqsFilePath == ""? "./data/s_for_p_and_q" : pqsFilePath;
-  std::ofstream pqsFile(pqsFilePath, std::ios::binary);
-  for (std::set<uint16_t>::iterator p = primes.begin(); p != beforeEnd; p++) {
+  std::stringstream ss;
+  ss << pqsFilePath << "_" << pFrom << "_" << pUntil;
+  std::ofstream pqsFile(ss.str().c_str(), std::ios::binary);
+
+  for (std::set<uint16_t>::iterator p = ipFrom; p != ipUntil; p++) {
     for (std::set<uint16_t>::iterator q = std::next(p, 1); q != end; q++) {
       uint32_t m = *p * *q;
       uint16_t p_temp = *p;
@@ -74,14 +81,16 @@ void read(const char * pqsFilePath="") {
 }
 
 int main(int argc, char const *argv[]) {
+  uint16_t pFrom = atoi(argv[1]);
+  uint16_t pUntil = atoi(argv[2]);
   // argv[1] for PQS FILE PATH
   // argv[2] for PRIME FILE PATH
-  if (argc == 1) {
-    generate();
-  } else if (argc == 2) {
-    generate(argv[1]);
-  } if (argc == 3) {
-    generate(argv[1], argv[2]);
+  if (argc == 3) {
+    generate(pFrom, pUntil);
+  } else if (argc == 4) {
+    generate(pFrom, pUntil, argv[3]);
+  } if (argc == 5) {
+    generate(pFrom, pUntil, argv[3], argv[4]);
   }
   return 0;
 }
