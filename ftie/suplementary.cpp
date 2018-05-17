40d424fcf9efaf16237a8c57e04c0b6c15fbaef6
Satria H R Harsono <HafizhHarsono@gmail.com>
2018-05-17 20:35:01 +0700

    Add iroot to suplementary

---
+++ ftie/suplementary.cpp
@@ -0,0 +1,14 @@
+#include "prime.h"
+
+#include <cmath>
+
+uint32_t iroot(uint64_t n) {
+  // https://en.wikipedia.org/wiki/Integer_square_root#Algorithm_using_Newton's_method
+  long double xk = n * 1.0;
+  long double xkp1 = (xk + (n / xk)) / 2.0;
+  while (std::fabs(xkp1 - xk) >= 1) {
+    xk = xkp1;
+    xkp1 = (xk + (n / xk)) / 2.0;
+  }
+  return uint32_t(xkp1);
+}
---
+++ ftie/suplementary.h
@@ -0,0 +1,8 @@
+#ifndef SUPLEMENTARY_H
+#define SUPLEMENTARY_H
+
+#include <cstdint>
+
+uint32_t iroot(uint64_t n);
+
+#endif
