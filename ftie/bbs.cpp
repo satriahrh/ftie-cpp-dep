4fddcd7aa1cfa48aed72dda32d38cb8351735771
Satria H R Harsono <hafizhharsono@gmail.com>
2018-05-26 18:49:14 +0700

    Simplified x ^ 2 mod m

--- ftie/bbs.cpp
+++ ftie/bbs.cpp
@@ -38,8 +38,7 @@ bbs::bbs(uint16_t p, uint16_t q, uint32_t s) {
 std::vector<uint8_t> bbs::generate_keystream(uint_fast32_t n) {
   std::vector<uint8_t> keystream(n);
   for(uint_fast32_t i = 0; i < n; i++) {
-    uint64_t x_64 = x * x % m;
-    x = x_64;
+    x = x * x % m;
     keystream[i] = x;
   }
   return keystream;
