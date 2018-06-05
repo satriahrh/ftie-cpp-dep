e72eb02ba32a607c039f4da42a802118f817882f
Satria H R Harsono <hafizhharsono@gmail.com>
2018-06-05 13:37:41 +0700

    namespace in bbs

--- ftie/bbs.cpp
+++ ftie/bbs.cpp
@@ -4,21 +4,19 @@
 #include <vector>
 
 
-bbs::bbs(){
-  m = 21;
-  x = 4;
-}
-
-bbs::bbs(uint16_t p, uint16_t q, uint32_t s) {
-  m = p * q;
-  x = s;
-}
-
-std::vector<uint8_t> bbs::generate_keystream(uint_fast32_t n) {
-  std::vector<uint8_t> keystream(n);
-  for(uint_fast32_t i = 0; i < n; i++) {
-    x = x * x % m;
-    keystream[i] = x;
+namespace ftie {
+  namespace bbs {
+    std::vector<uint8_t> generate_keystream(
+      uint16_t p, uint16_t q, uint32_t s, uint_fast32_t n
+    ) {
+      uint64_t m = p * q;
+      uint64_t x = s;
+      std::vector<uint8_t> keystream(n);
+      for(uint_fast32_t i = 0; i < n; i++) {
+        x = x * x % m;\
+        keystream[i] = x;
+      }
+      return keystream;
+    }
   }
-  return keystream;
 }
