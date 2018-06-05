ca5b2d3e5829a45a1f0c8524a9925ac3998c6a8f
Satria H R Harsono <hafizhharsono@gmail.com>
2018-06-05 14:19:14 +0700

    namespace in ftie

--- ftie/ftie.h
+++ ftie/ftie.h
@@ -9,18 +9,7 @@
 
 #include <vector>
 
-
-class ftie {
-private:
-  std::vector<uint8_t> physical_file_to_bytes_sequence(const char* filePath);
-  std::vector<uint8_t> bytes_sequence_padding(std::vector<uint8_t> plainbytes);
-  png::image<png::rgb_pixel> bytes_sequence_to_image(std::vector<uint8_t> cipherbytes);
-
-  std::vector<uint8_t> image_to_bytes_sequence(png::image<png::rgb_pixel> cipherimage);
-  std::vector<uint8_t> bytes_sequence_stripping(std::vector<uint8_t> plainbytes);
-  void bytes_sequence_to_physical_file(std::vector<uint8_t>, const char* filePath);
-
-public:
+namespace ftie {
   void encrypt(
     uint16_t p, uint16_t q, uint32_t s, uint_fast16_t a, uint_fast16_t b, uint_fast16_t n,
     const char* plainfileFilepath, const char* cipherimageFilepath
@@ -29,6 +18,19 @@ public:
     uint16_t p, uint16_t q, uint32_t s, uint_fast16_t a, uint_fast16_t b, uint_fast16_t n,
     const char* cipherimageFilepath, const char* plainfileFilepath
   );
-};
+}
+// class ftie {
+// private:
+//   std::vector<uint8_t> physical_file_to_bytes_sequence(const char* filePath);
+//   std::vector<uint8_t> bytes_sequence_padding(std::vector<uint8_t> plainbytes);
+//   png::image<png::rgb_pixel> bytes_sequence_to_image(std::vector<uint8_t> cipherbytes);
+//
+//   std::vector<uint8_t> image_to_bytes_sequence(png::image<png::rgb_pixel> cipherimage);
+//   std::vector<uint8_t> bytes_sequence_stripping(std::vector<uint8_t> plainbytes);
+//   void bytes_sequence_to_physical_file(std::vector<uint8_t>, const char* filePath);
+//
+// public:
+//
+// };
 
 #endif
