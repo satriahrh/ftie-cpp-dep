e73beeb933f0cf948a225a4269cd30cc2648f182
Satria H R Harsono <hafizhharsono@gmail.com>
2018-06-07 13:43:05 +0700

    apply bbs validation

--- ftie/bbs.cpp
+++ ftie/bbs.cpp
@@ -1,6 +1,9 @@
 #include "bbs.h"
 
+#include "prime.h"
+
 #include <cstdint>
+#include <numeric>
 #include <vector>
 
 
@@ -9,7 +12,26 @@ namespace ftie {
     std::vector<uint8_t> generate_keystream(
       uint16_t p, uint16_t q, uint32_t s, uint_fast32_t n
     ) {
+      {
+        if (p < q) {
+          uint16_t temp = q;
+          q = p;
+          p = temp;
+        }
+        prime prm(p + 1);
+        if (!prm.is_prime(p) || !prm.is_prime(q))
+          throw "p or q is not prime";
+      }
+      if (!(p % 4 == 3) || !(q % 4 == 3))
+        throw "p or q is not congruence to 3 mod 4";
+        
       uint64_t m = p * q;
+
+      if (!(1 < s) || !(s < m))
+        throw "s is not in (1, m)";
+      if (std::gcd(s, m) != 1)
+        throw "gcd(s,m) != 1";
+
       uint64_t x = s;
       std::vector<uint8_t> keystream(n);
       for(uint_fast32_t i = 0; i < n; i++) {
