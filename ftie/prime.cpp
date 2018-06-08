6643aa83b2bb6e7873067de8d46737959251b865
Satria H R Harsono <hafizhharsono@gmail.com>
2018-06-08 15:37:18 +0700

    uint_fast16_t to uint16_t

--- ftie/prime.cpp
+++ ftie/prime.cpp
@@ -4,7 +4,7 @@
 #include <cstdint>
 
 
-uint_fast16_t iroot(uint_fast16_t n) {
+uint16_t iroot(uint16_t n) {
   // https://en.wikipedia.org/wiki/Integer_square_root#Algorithm_using_Newton's_method
   float xk = n * 1.0;
   float xkp1 = (xk + (n / xk)) / 2.0;
@@ -12,15 +12,15 @@ uint_fast16_t iroot(uint_fast16_t n) {
     xk = xkp1;
     xkp1 = (xk + (n / xk)) / 2.0;
   }
-  return uint_fast16_t(xkp1);
+  return uint16_t(xkp1);
 }
 
-prime::prime(uint_fast16_t maximumN) {
+prime::prime(uint16_t maximumN) {
   N = maximumN;
   // sieve of eratosthenes
   primes = new bool[N];
-  uint_fast16_t limit = iroot(N);
-  for(uint_fast16_t i = 2; i <= limit; i++)
+  uint16_t limit = iroot(N);
+  for(uint16_t i = 2; i <= limit; i++)
     if (!primes[i])
       for(uint16_t j = i + i; j < N; j += i)
         primes[j] = true;
@@ -30,6 +30,6 @@ prime::~prime() {
   delete []primes;
 }
 
-bool prime::is_prime(uint_fast16_t n) {
+bool prime::is_prime(uint16_t n) {
   return n >= N ? false : !primes[n];
 }
