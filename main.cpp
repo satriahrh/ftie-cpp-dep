8b4379cfa56d4c4252111167ff4127944eda2dd6
Satria H R Harsono <hafizhharsono@gmail.com>
2018-06-11 11:04:33 +0700

    Bug in S

--- main.cpp
+++ main.cpp
@@ -16,7 +16,7 @@ int main(int argc, char* argv[]) {
   const char* OUT_FILE_PATH = argv[3];
   uint16_t P = atoi(argv[4]);
   uint16_t Q = atoi(argv[5]);
-  uint64_t S = atoi(argv[6]);
+  uint32_t S = atoi(argv[6]);
   uint16_t A = atoi(argv[7]);
   uint16_t B = atoi(argv[8]);
   uint16_t N = atoi(argv[9]);
